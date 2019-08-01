b53a39a0551d9ce89bbfe48f00745d2772ee9ca8
lalala <boqianfan@gmail.com>
2019-07-31 23:32:15 -0400

    Update reverseWord.py

--- reverseWord.py
+++ reverseWord.py
@@ -6,3 +6,22 @@ class Solution:
     def reverseWords(self, s):
         # write your code here
         return " ".join(s.split()[::-1])
+    
+    """
+    Solution2
+    @param: s: A string
+    @return: A string
+    """
+    def reverseWord(str):
+        lStr = list(reversed(str))
+        size = len(lStr)
+        i = 0
+        while i < size:
+            j = 0
+            while i < size and lStr[i] == ' ':
+                i = i + 1
+            while i + j < size and lStr[i + j] != ' ':
+                j = j + 1
+            lStr[i:i+j] = list(reversed(lStr[i:i+j]))
+            i = i + j
+        return "".join(lStr)
